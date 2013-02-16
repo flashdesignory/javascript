9f884a61a6fe77109f5447d619e3e67f546b86f3
Thorsten Kober <info@flashdesignory.com>
2013-02-16 07:02:23 -0800

    Create debug.js

---
+++ debug.js
@@ -0,0 +1,32 @@
+/*
+ * @title: Debug
+ * @description: simple debug tool
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+ 
+var Debug = (function(window) {
+
+  var _debug = true;
+
+	return{
+		getDebug: function(){
+			return _debug;
+		},
+		setDebug: function(value){
+			_debug = value;
+		},
+		log: function(channel, message){
+			if(!_debug) return;
+
+			if(window.console) {
+				try {
+					console.log(channel + ": " + message);
+				} catch(e) {
+					//do something smart here..
+				}
+			}
+		}
+	};
+
+})(window);
