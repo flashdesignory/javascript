77bf0e3be371ac8c5a6f8b06a21fed45807f85f5
Thorsten Kober <thorsten@thorstenkober.com>
2017-09-28 17:54:56 -0700

    added stack / queue

---
+++ datastructures/queue.js
@@ -0,0 +1,39 @@
+/*
+ * @title: Queue
+ * @description: Generic Stack Class
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+//first-in first-out (FIFO)
+
+function Queue(){
+	this.data = {};
+	this.first = 0;
+	this.last = 0;
+}
+
+Queue.prototype.push = function(value){
+	this.data[this.last] = value;
+	this.last++;
+}
+
+Queue.prototype.shift = function(){
+	var temp = this.data[this.first];
+	delete this.data[this.first];
+	this.first++;
+	return temp;
+}
+
+Queue.prototype.peek = function(){
+	return this.data[this.first];
+}
+
+//example
+var queue = new Queue();
+queue.push(1);
+queue.push(2);
+queue.push(3);
+console.log(queue.peek());
+console.log(queue.shift());
+console.log(queue.peek());
---
+++ datastructures/stack.js
@@ -0,0 +1,38 @@
+/*
+ * @title: Stack
+ * @description: Generic Stack Class
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+//last-in first-out (LIFO)
+
+ function Stack(){
+ 	this.data = {};
+ 	this.size = 0;
+ }
+
+ Stack.prototype.push = function(value){
+ 	this.data[this.size] = value;
+ 	this.size++;
+ }
+
+ Stack.prototype.pop = function(){
+ 	var temp = this.data[this.size-1];
+ 	delete this.data[this.size-1];
+ 	this.size--;
+ 	return temp;
+ }
+
+ Stack.prototype.peek = function(){
+ 	return this.data[this.size-1];
+ }
+
+//example
+var stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
