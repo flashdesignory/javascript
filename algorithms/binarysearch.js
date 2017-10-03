bcc811eaeb0eb339371505593d19d4ff4f6a92f1
Thorsten Kober <thorsten@thorstenkober.com>
2017-10-02 22:22:43 -0700

    modified example

--- algorithms/binarysearch.js
+++ algorithms/binarysearch.js
@@ -24,8 +24,6 @@ function binarySearch(arr, item){
   return -1;
 }
 
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-					41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 var result = binarySearch(primes, 53);
 console.log(result);
