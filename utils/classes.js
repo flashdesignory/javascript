df23f7eeaf889e029aa2abe70cc63f2279b0611d
Thorsten Kober <thorsten@thorstenkober.com>
2017-10-03 21:52:06 -0700

    added more utils

---
+++ utils/classes.js
@@ -0,0 +1,34 @@
+var Classes = (function(){
+	return {
+		addClass:function(element, value){
+			if(element){
+				if(element.classList){
+					var classes = element.classList;
+					if(!classes.contains(value)){
+						classes.add(value);
+					}
+				}
+			}
+		},
+		removeClass:function(element, value){
+			if(element){
+				if(element.classList){
+					var classes = element.classList;
+					classes.remove(value);
+				}
+			}
+		},
+		hasClass:function(element, value){
+			var temp = false;
+			if(element){
+				if(element.classList){
+					var classes = element.classList;
+					if(classes.contains(value)){
+						temp = true;
+					}
+				}
+			}
+			return temp;
+		}
+	}
+})();
