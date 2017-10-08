14371a01b4de5a3362de2fea8667a4d757924422
Thorsten Kober <thorsten@thorstenkober.com>
2017-10-08 10:33:22 -0700

    rotate array added

---
+++ algorithms/array.rotate.js
@@ -0,0 +1,67 @@
+/*
+ * @title: Rotate Array
+ * @description: Simple function to rotate array
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+//verification of input:
+//if(isNaN(k) || k < 0) return;
+//if(arr.length == 0 ) return;
+//if(k > arr.length)k = k%arr.length; 
+
+var nums = [1,2,3,4,5,6,7,8];             
+
+function rotateArray_one(arr, k){
+	var result = [];
+	var i;
+
+	for(i = 0; i<k; i++){
+		//console.log(arr[arr.length-k+i]);
+		result.push(arr[arr.length-k+i]);
+	}
+
+	for(i = 0; i<arr.length-k; i++){
+		//console.log(arr[i]);
+		result.push(arr[i]);
+	}
+
+	return result;
+}
+
+console.log(rotateArray_one(nums, 3));
+
+function rotateArray_two(arr, k){
+	var result = [];
+	var i;
+	var index = 0;
+
+	for(i = 0; i<k; i++){
+		//console.log(arr[arr.length-k+i]);
+    	result[i] = arr[arr.length-k+i];
+	}
+
+	for(i = k; i<arr.length; i++){
+		//console.log(arr[index]);
+		result[i] = arr[index];
+		index++;
+	}
+
+	return result;
+}
+
+console.log(rotateArray_two(nums, 3));
+
+function rotateArray_three(arr, k){
+	for(var i = 0; i<k; i++){
+		for(var j = arr.length-1; j> 0; j--){
+			var temp = arr[j];
+			arr[j] = arr[j-1];
+			arr[j-1] = temp;
+		}
+	}
+
+	return arr;
+}
+
+console.log(rotateArray_three(nums, 3));
