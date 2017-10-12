ccb1b48e97ba78dc51a2b3dd5d84b55f469d900d
Thorsten Kober <thorsten@thorstenkober.com>
2017-10-12 11:16:26 -0700

    more algorithms

---
+++ algorithms/number.swap.js
@@ -0,0 +1,25 @@
+/*
+ * @title: Swap Number
+ * @description: simple functions to swap numbers
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+ function swapNumbers_one(a,b){
+ 	b = b-a;
+ 	a = a+b;
+ 	b = a-b;
+ 	return [a,b];
+ }
+
+ console.log(swapNumbers_one(3,5));
+
+ function swapNumbers_two(a,b){
+ 	a = a^b;
+ 	b = a^b;
+ 	a = a^b;
+ 	return [a,b];
+ }
+
+ console.log(swapNumbers_two(4,6));
+ 
---
+++ algorithms/string.reverse.js
@@ -0,0 +1,59 @@
+/*
+ * @title: Reverse String
+ * @description: Simple function to reverse string
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+ function reverseString_one(str){
+  var result = "";
+  for(var i = str.length-1; i>=0; i--){
+    result += str[i];
+  }
+  return result;
+}
+
+console.log(reverseString_one("hello"));
+
+function reverseString_two(str){
+  var middle = Math.floor(str.length/2);
+  var chars = str.split("");
+  
+  for(var i = 0; i<middle; i++){
+    var temp = chars[i];
+    chars[i] = chars[chars.length-1-i];
+    chars[chars.length-1-i] = temp;
+  }
+  
+  return chars.join("");
+}
+
+console.log(reverseString_two("hello"));
+
+function reverseString_three(str){
+  var chars = str.split("");
+  var left = 0;
+  var right = chars.length-1;
+  
+  while(left < right){
+    var temp = chars[left];
+    chars[left] = chars[right];
+    chars[right] = temp;
+    left++;
+    right--;
+  }
+  
+  return chars.join("");
+}
+
+console.log(reverseString_three("hello"));
+
+function reverseString_four(str){
+  if(str === ""){
+    return "";
+  }else{
+    return reverseString_four(str.substr(1)) + str.charAt(0);
+  }
+}
+
+console.log(reverseString_four("hello"));
