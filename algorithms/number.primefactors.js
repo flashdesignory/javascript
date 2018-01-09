744247b9a944fcc0037485f004f841311c936523
Thorsten Kober <info@flashdesignory.com>
2018-01-08 16:23:07 -0800

    added function to find primes of number

---
+++ algorithms/number.primefactors.js
@@ -0,0 +1,24 @@
+/*
+ * @title: Prime Factors
+ * @description: find prime factors of given number and return
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+ function findPrimes(n){
+ 	var factors = [];
+ 	var divisor = 2;
+
+ 	while(n > 2){
+ 		if(n % divisor === 0){
+ 			factors.push(divisor);
+ 			n = n/divisor;
+ 		}else{
+ 			divisor ++;
+ 		}
+ 	}
+
+ 	return factors;
+ }
+
+ console.log(findPrimes(69));//[3,23];
