8f297d776afbbdf3d3a3137f25fba2f811a3e5cb
Thorsten Kober <info@flashdesignory.com>
2018-01-09 14:00:56 -0800

    modified combinations" "

--- algorithms/number.combination.js
+++ algorithms/number.combination.js
@@ -6,22 +6,44 @@
  */
 
  //itteration
- function combinations(arr){
+ function combinations(str){
  	var result = [];
 
- 	for(var i = 0; i<arr.length; i++){
- 		var item_one = arr[i];
- 		for(var j = i+1; j<arr.length; j++){
- 			var item_two = arr[j];
- 			result.push([item_one, item_two]);
+ 	for(var i = 0; i<str.length; i++){
+ 		var resultLength = result.length;
+ 		for(var j = 0; j<resultLength; j++){
+ 			result.push(str[i] + result[j]);
  		}
+ 		result.push(str[i]);
  	}
 
+ 	console.log(result.length);
  	return result;
  }
 
  //example
- console.log(combinations(["a","b","c","d"]));
+ console.log(combinations("abcd"));
+
+ //recursive
+ function combinations_two(str){
+ 	var result = [];
+
+ 	function combine(used, unused){
+ 		if(!used && !unused) return;
+ 		if(!unused) result.push(used);
+ 		else{
+ 			combine(used + unused[0], unused.slice(1));
+ 			combine(used, unused.slice(1));
+ 		}
+ 	}
+
+ 	combine("", str);
+ 	console.log(result.length);
+ 	return result;
+ }
+
+ //example
+ console.log(combinations_two("abcd"));
 
  //recursive with factorial
  //Formula: n! / k!(n- k)!
