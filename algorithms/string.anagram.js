05d9b6cc8d569c81da7f81bb7be15785b83143a7
Thorsten Kober <info@flashdesignory.com>
2018-01-20 18:18:56 -0800

    added anagram

---
+++ algorithms/string.anagram.js
@@ -0,0 +1,29 @@
+/*
+ * @title: Anagram
+ * @description: Simple function to validate an anagram
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+function anagram(s,t){
+  if(s.length != t.length) return false;
+  var found = {};
+  var i;
+  
+  for(i = 0; i<s.length; i++){
+    if(!found[s[i]]) found[s[i]] = 1;
+    else found[s[i]] += 1;
+  }
+  
+  for(i = 0; i<t.length; i++){
+    if(!found[t[i]]) return false;
+    else found[t[i]] -= 1;
+    
+    if(found[t[i]] < 0) return false;
+  }
+  
+  return true;
+}
+
+console.log(anagram("anagram", "nagaram")); //true
+console.log(anagram("rat", "car")); //false
