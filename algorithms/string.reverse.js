71fd19ebbd8112cab61adcc1b59128ce9a871189
Thorsten Kober <thor@part4.com>
2018-08-29 18:14:23 -0700

    added additional string reverse solution

--- algorithms/string.reverse.js
+++ algorithms/string.reverse.js
@@ -5,9 +5,9 @@
  * @email: info@flashdesignory.com
  */
 
- function reverseString_one(str){
+function reverseString_one(str) {
   var result = "";
-  for(var i = str.length-1; i>=0; i--){
+  for (var i = str.length - 1; i >= 0; i--) {
     result += str[i];
   }
   return result;
@@ -15,45 +15,57 @@
 
 console.log(reverseString_one("hello"));
 
-function reverseString_two(str){
-  var middle = Math.floor(str.length/2);
+function reverseString_two(str) {
+  var middle = Math.floor(str.length / 2);
   var chars = str.split("");
-  
-  for(var i = 0; i<middle; i++){
+
+  for (var i = 0; i < middle; i++) {
     var temp = chars[i];
-    chars[i] = chars[chars.length-1-i];
-    chars[chars.length-1-i] = temp;
+    chars[i] = chars[chars.length - 1 - i];
+    chars[chars.length - 1 - i] = temp;
   }
-  
+
   return chars.join("");
 }
 
 console.log(reverseString_two("hello"));
 
-function reverseString_three(str){
+function reverseString_three(str) {
   var chars = str.split("");
   var left = 0;
-  var right = chars.length-1;
-  
-  while(left < right){
+  var right = chars.length - 1;
+
+  while (left < right) {
     var temp = chars[left];
     chars[left] = chars[right];
     chars[right] = temp;
     left++;
     right--;
   }
-  
+
   return chars.join("");
 }
 
 console.log(reverseString_three("hello"));
 
-function reverseString_four(str){
-  if(str === ""){
+function reverseString_four(str) {
+  if (str === "") {
     return "";
-  }else{
+  } else {
     return reverseString_four(str.substr(1)) + str.charAt(0);
   }
 }
 
 console.log(reverseString_four("hello"));
+
+function reverseString_five(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+
+  var left = str[0];
+  var right = str[str.length - 1];
+  return right + reverseString_five(str.substring(1, str.length - 1)) + left;
+}
+
+console.log(reverseString_five("hello"));
