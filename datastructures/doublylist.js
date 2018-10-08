ebafa772a922178279f1f0d9441cf80b92e9f866
Thorsten Kober <info@flashdesignory.com>
2018-10-08 13:04:36 -0700

    added reverse to doublylist

--- datastructures/doublylist.js
+++ datastructures/doublylist.js
@@ -18,16 +18,16 @@ function DoublyList(){
 
 DoublyList.prototype.log = function(direction){
   if(!this.head) return;
-  
+
   var values = [];
   var current;
-  
+
   if(direction === undefined) direction = "forwards";
-  
+
   if(direction == "forwards"){
     current = this.head;
     values.push(current.value);
-  
+
     while(current.next){
       current = current.next;
       values.push(current.value);
@@ -35,19 +35,19 @@ DoublyList.prototype.log = function(direction){
   }else{
     current = this.tail;
     values.push(current.value);
-    
+
     while(current.previous){
       current = current.previous;
       values.push(current.value);
     }
   }
-  
+
   console.log(values);
 }
 
 DoublyList.prototype.add = function(value){
   var node = new Node(value);
-  
+
   if(!this.head){
     this.head = node;
     this.tail = node;
@@ -62,16 +62,16 @@ DoublyList.prototype.add = function(value){
 
 DoublyList.prototype.remove = function(value){
   if(!this.head) return;
-  
+
   var current = this.head;
   var previous = this.head;
-  
+
   if(current.value == value){
     this.head = current.next;
     if(this.head) this.head.previous = null;
     return current;
   }
-  
+
   while(current.next){
     if(current.value == value){
       previous.next = current.next;
@@ -82,13 +82,27 @@ DoublyList.prototype.remove = function(value){
       current = current.next;
     }
   }
-  
+
   if(current.value == value){
     previous.next = null;
     this.tail = previous;
   }
-  
+
   return;
+}
+
+DoublyList.prototype.reverse = function(){
+  var current = this.head;
+  var previous = null;
+  this.head = this.tail;
+  this.tail = current;
+
+  while(current){
+    previous = current.previous;
+    current.previous = current.next;
+    current.next = previous;
+    current = current.previous;
+  }
 }
 
 var list = new DoublyList();
@@ -98,6 +112,8 @@ list.add(3);
 list.add(4);
 list.add(5);
 list.log();
-list.remove(5);
-list.log("forwards");
-list.log("reverse");
+//list.remove(5);
+//list.log("forwards");
+//list.log("reverse");
+list.reverse();
+list.log();
