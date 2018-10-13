1ece6a889aeab5e072d531c0bb436be75234e26f
Thorsten Kober <info@flashdesignory.com>
2018-10-13 07:57:29 -0700

    adjusted graph class

--- datastructures/graph.js
+++ datastructures/graph.js
@@ -5,81 +5,117 @@
  * @email: info@flashdesignory.com
  */
 
- //IN PROGRESS
- 
- function Graph(){
- 	this.vertices = [];
- 	this.edges = [];
- 	this.numEdges = 0;
+ class Queue{
+   constructor(){
+     this.storage = {};
+     this.first = 0;
+     this.last = 0;
+   }
+   enqueue(value){
+     this.storage[this.last] = value;
+     this.last++;
+   }
+   dequeue(){
+     let temp = this.storage[this.first];
+     delete this.storage[this.first];
+     this.first++;
+     return temp;
+   }
+   empty(){
+     return this.first === this.last;
+   }
+   peek(){
+     return this.storage[this.first];
+   }
  }
 
- Graph.prototype.addVertex = function(vertex){
- 	this.vertices.push(vertex);
- 	this.edges[vertex] = [];
+ class Graph{
+   constructor(){
+     this.numVertices = 0;
+     this.adjList = {};
+   }
+   print(){
+     let vertices = Object.keys(this.adjList);
+     for(let i = 0; i<vertices.length; i++){
+       let result = vertices[i] + " -> ";
+       let edges = this.adjList[vertices[i]];
+       for(let i = 0; i<edges.length; i++){
+         result += edges[i] + ",";
+       }
+       console.log(result);
+     }
+   }
+   contains(v){
+     return !!this.adjList[v];
+   }
+   addVertex(v){
+     this.numVertices++;
+     this.adjList[v] = [];
+   }
+   addEdge(one, two){
+     this.adjList[one].push(two);
+     this.adjList[two].push(one);
+   }
+   bfs(v){
+     let visited = [];
+     for(let i = 0; i<this.numVertices; i++){
+       visited[i] = false;
+     }
+
+     let q = new Queue();
+     q.enqueue(v);
+     visited[v] = true;
+
+     while(!q.empty()){
+       let current = q.dequeue();
+       console.log(current);
+       let edges = this.adjList[current];
+       for(let i = 0; i<edges.length; i++){
+         let edge = edges[i];
+         if(!visited[edge]){
+           visited[edge] = true;
+           q.enqueue(edge);
+         }
+       }
+     }
+   }
+   dfs(v){
+     let visited = [];
+     for(let i = 0; i<this.numVertices; i++){
+       visited[i] = false;
+     }
+     this.traverse(v, visited);
+   }
+   traverse(v, visited){
+     visited[v] = true;
+     console.log(v);
+     let edges = this.adjList[v];
+     for(let i = 0; i<edges.length; i++){
+       let edge = edges[i];
+       if(!visited[edge]){
+         this.traverse(edge, visited);
+       }
+     }
+   }
  }
 
- Graph.prototype.removeVertex = function(vertex){
- 	var index = this.vertices.indexOf(vertex);
-
- 	if(index != -1){
- 		this.vertices.splice(index, 1);
- 	}
-
- 	while(this.edges[vertex].length){
- 		var adjacentVertex = this.edges[vertex].pop();
- 		this.removeEdge(adjacentVertex, vertex);
- 	}
- }
-
- Graph.prototype.addEdge = function(vertex1, vertex2){
- 	this.edges[vertex1].push(vertex2);
- 	this.edges[vertex2].push(vertex1);
- 	this.numEdges++;
- }
-
- Graph.prototype.removeEdge = function(vertex1, vertex2){
- 	var index1 = this.edges[vertex1] ? this.edges[vertex1].indexOf(vertex2) : -1;
- 	var index2 = this.edges[vertex2] ? this.edges[vertex2].indexOf(vertex1) : -1;
-
- 	if(index1 != -1){
- 		this.edges[vertex1].splice(index1, 1);
- 		this.numEdges--;
- 	}
- 	if(index2 != -1){
- 		this.edges[vertex2].splice(index2, 1);
- 	}
- }
-
- Graph.prototype.size = function(){
- 	return this.vertices.length;
- }
-
- Graph.prototype.relations = function(){
- 	return this.numEdges;
- }
-
- Graph.prototype.print = function(){
- 	//for(var i = 0; i<this.vertices.length; i++){
- 		//console.log(this.vertices[i]);
- //	}
-   console.log(this.vertices.join(', '));
- }
-
-var graph = new Graph();
-graph.addVertex(1);
-graph.addVertex(2);
-graph.addVertex(3);
-graph.addVertex(4);
-graph.addVertex(5);
-graph.addVertex(6);
-graph.print(); // 1 -> | 2 -> | 3 -> | 4 -> | 5 -> | 6 ->
-graph.addEdge(1, 2);
-graph.addEdge(1, 5);
-graph.addEdge(2, 3);
-graph.addEdge(2, 5);
-graph.addEdge(3, 4);
-graph.addEdge(4, 5);
-graph.addEdge(4, 6);
-graph.print(); // 1 -> 2, 5 | 2 -> 1, 3, 5 | 3 -> 2, 4 | 4 -> 3, 5, 6 | 5 -> 1, 2, 4 | 6 -> 4
-console.log('graph size (number of vertices):', graph.size()); // => 6
-console.log('graph relations (number of edges):', graph.relations()); // => 7
+ let g = new Graph();
+ g.addVertex("a");
+ g.addVertex("b");
+ g.addVertex("c");
+ g.addVertex("d");
+ g.addVertex("e");
+ g.addVertex("f");
+ g.addEdge("a", "b");
+ g.addEdge("a", "d");
+ g.addEdge("a", "e");
+ g.addEdge("b", "c");
+ g.addEdge("d", "e");
+ g.addEdge("e", "f");
+ g.addEdge("e", "c");
+ g.addEdge("c", "f");
+ g.print();
+ console.log("************");
+ g.bfs('a');
+ console.log("************");
+ g.dfs('a');
