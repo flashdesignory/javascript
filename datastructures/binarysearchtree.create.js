03869206fbe3941ed7d9e7600361ed4c618ffc22
Thorsten Kober <info@flashdesignory.com>
2018-10-17 12:27:38 -0700

    added list to bst creation

--- datastructures/binarysearchtree.create.js
+++ datastructures/binarysearchtree.create.js
@@ -24,4 +24,89 @@ function createFromArray(arr, start, end){
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
 let root = createFromArray(arr, 0, arr.length-1);
+console.log("bst from array:");
 console.log(root);
+console.log("********************************************");
+
+/*
+ * @title: Binary Search Tree
+ * @description: create from linked List;
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+class ListNode{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+  serialize(){
+    let result = {};
+    result.value = this.value;
+    result.next = this.next ? this.next.serialize() : null;
+    return result;
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
+  add(value){
+    let node = new ListNode(value);
+    if(!this.head){
+      this.head = node;
+      return node;
+    }
+
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+    return node;
+  }
+  length(){
+    if(!this.head) return 0;
+    let current = this.head;
+    let length = -1;
+    while(current){
+      length++;
+      current = current.next;
+    }
+    return length;
+  }
+}
+
+let list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+list.add(6);
+list.add(7);
+list.add(8);
+list.add(9);
+list.add(10);
+
+function createFromLinkedList(list){
+  let n = list.length();
+
+  function buildFromList(n){
+    if(n <= 0) return null;
+    let middle = Math.floor(n/2);
+    let left = buildFromList(middle);
+    let node = new Node(list.head.value);
+    node.left = left;
+    list.head = list.head.next;
+    node.right = buildFromList(n-middle-1);
+    return node;
+  }
+  return buildFromList(n);
+}
+
+root = createFromLinkedList(list);
+console.log("bst from list:");
+console.log(root);
+console.log("********************************************");
