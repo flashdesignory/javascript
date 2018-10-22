7bf1099c986b513c65c72e1dcde1363b69c73192
Thorsten Kober <thor@part4.com>
2018-10-22 14:34:11 -0700

    added matrix traversal

---
+++ algorithms/matrix.traverse.js
@@ -0,0 +1,39 @@
+/*
+ * @title: Traverse Matrix
+ * @description: traverse matrix, alternate col direction
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+let matrix = [[ 1, 8, 9,16,17],
+              [ 2, 7,10,15,18],
+              [ 3, 6,11,14,19],
+              [ 4, 5,12,13,20]]
+
+function print(numRows, numCols){
+  let matrix = [];
+  for(let i = 0; i<numRows; i++){
+    for(let i = 0; i<numRows; i++){
+      matrix[i] = [];
+    }
+  }
+
+  let count = 1;
+
+  for(let i = 0; i<numCols; i++){
+    let even = i%2;
+    if(even){
+      for(let j = numRows-1; j>=0; j--){
+        matrix[j][i] = count++;
+      }
+    }else{
+      for(let j = 0; j<numRows; j++){
+        matrix[j][i] = count++;
+      }
+    }
+  }
+
+  console.log(matrix);
+}
+
+print(5,4)
