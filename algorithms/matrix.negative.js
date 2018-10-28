507ec19ce11340e2a5aa801ca5932f2147d7fed9
Thorsten Kober <info@flashdesignory.com>
2018-10-28 11:29:00 -0700

    added algo

---
+++ algorithms/matrix.negative.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Count Negative Matrix
+ * @description: find all negative cells
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+ const tiles = [[-4,-3,-1,1],
+                [-2,-2,1,2],
+                [-1,1,2,3],
+                [0,1,2,3]];
+
+function countNegativeTiles(matrix){
+  let row = 0;
+  let col = matrix[0].length-1;
+  let count = 0;
+
+  while(row < matrix.length && col >= 0){
+    if(matrix[row][col] < 0){
+      count += (col + 1);
+      row++;
+      if(matrix[row]){ //go down a row and go left if already negative
+        while(matrix[row][col] < 0) {
+          col++;
+        }
+      }
+    }else{
+      col--;
+    }
+  }
+  return count;
+}
+
+countNegativeTiles(tiles);
