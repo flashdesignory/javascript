1aa379749d98727309820a55e924b5c5db81aa6b
Thorsten Kober <info@flashdesignory.com>
2018-10-30 22:40:20 -0700

    cleanup oneedit

--- algorithms/string.onedit.js
+++ algorithms/string.onedit.js
@@ -5,42 +5,44 @@
  * @email: info@flashdesignory.com
  */
 
-function onEditAway(s, t) {
-  if (s.length > t.length) {
-    const temp = s;
-    s = t;
-    t = temp;
-  }
+function oneEditAway(str1, str2) {
+  const length1 = str1.length;
+  const length2 = str2.length;
 
-  if ((t.length - s.length) > 1) {
-    return false;
-  }
+  if (Math.abs(length1 - length2) > 1) return false;
 
-  let numDifferent = 0;
-  let i = 0;
-  let j = 0;
+  let index1 = 0;
+  let index2 = 0;
+  let numEdits = 0;
 
-  while (i < s.length && j < t.length) {
-    if (s[i] !== t[j]) {
-      if (numDifferent !== 0) {
-        return false;
+  while (index1 < length1 && index2 < length2) {
+    if (str1[index1] !== str2[index2]) {
+      if (numEdits >= 1) return false;
+
+      if (length1 > length2) {
+        index1++;
+      } else if (length2 > length1) {
+        index2++;
+      } else {
+        index1++;
+        index2++;
       }
-
-      numDifferent++;
-
-      if (t.length !== s.length) {
-        i--;
-      }
+      numEdits++;
+    } else {
+      index1++;
+      index2++;
     }
-
-    i++;
-    j++;
   }
 
-  return numDifferent === 1 || (t.length !== s.length && (t.length - j) === 1);
+  if (index1 < length1 || index2 < length2) {
+    numEdits++;
+  }
+
+  return numEdits === 1;
 }
 
-console.log(onEditAway('car', 'cart'));// true
-console.log(onEditAway('car', 'cto'));// false
-console.log(onEditAway('car', 'com'));// false;
-console.log(onEditAway('car', 'mar'));// true;
+console.log(oneEditAway('car', 'cart')); // true
+console.log(oneEditAway('car', 'cto')); // false
+console.log(oneEditAway('car', 'com')); // false;
+console.log(oneEditAway('car', 'mar')); // true;
+console.log(oneEditAway('pale', 'ple')); // true
