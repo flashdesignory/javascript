694bd0bfa827fc6793d6334333633851b4b4e24e
Thorsten Kober <info@flashdesignory.com>
2018-11-15 21:41:22 -0800

    added rpn lss

---
+++ algorithms/number.rpn.js
@@ -0,0 +1,64 @@
+/*
+ * @title: Reverse Polish Notation
+ * @description: reverse polish notation example
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+function reversePolishNotation(arr) {
+  if (arr.length <= 2) {
+    return null;
+  }
+
+  const operands = ['+', '-', '*', '/'];
+  const stack = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    const index = operands.indexOf(item);
+    if (index < 0) {
+      stack.push(arr[i]);
+    } else {
+      let a; let
+        b;
+      switch (index) {
+        case 0:
+          a = Number(stack.pop());
+          b = Number(stack.pop());
+          stack.push(a + b);
+          break;
+        case 1:
+          a = Number(stack.pop());
+          b = Number(stack.pop());
+          stack.push(b - a);
+          break;
+        case 2:
+          a = Number(stack.pop());
+          b = Number(stack.pop());
+          stack.push(a * b);
+          break;
+        case 3:
+          a = Number(stack.pop());
+          b = Number(stack.pop());
+          stack.push(b / a);
+          break;
+        default:
+      }
+    }
+  }
+  console.log(stack);
+  return parseInt(stack[0], 10);
+}
+
+// npx jest algorithms/number.rpn.js
+describe('reverse polish notation', () => {
+  test('reversePolishNotation()', () => {
+    expect(reversePolishNotation(['2', '1', '+', '3', '*'])).toEqual(9);
+  });
+  test('reversePolishNotation()', () => {
+    expect(reversePolishNotation(['4', '13', '5', '/', '+'])).toEqual(6);
+  });
+  test('reversePolishNotation()', () => {
+    expect(reversePolishNotation(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])).toEqual(22);
+  });
+});
---
+++ algorithms/string.sequence.js
@@ -0,0 +1,45 @@
+/*
+ * @title: String sequence
+ * @description: longest sequence of non-repeating characters
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+
+function longestSubSequence(str) {
+  const seen = {};
+  let count = 0;
+  let max = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if (!seen[current]) {
+      count++;
+      seen[current] = true;
+      max = Math.max(max, count);
+    } else {
+      count = 1;
+    }
+  }
+
+  return max;
+}
+
+// npx jest algorithms/string.sequence.js
+describe('longest string sequence', () => {
+  test("longestSubSequence('obamacare')", () => {
+    expect(longestSubSequence('obamacare')).toEqual(4);
+  });
+  test("longestSubSequence('bbb')", () => {
+    expect(longestSubSequence('bbb')).toEqual(1);
+  });
+  test("longestSubSequence('pwwke')", () => {
+    expect(longestSubSequence('pwwke')).toEqual(3);
+  });
+  test("longestSubSequence('abcabcbb')", () => {
+    expect(longestSubSequence('abcabcbb')).toEqual(3);
+  });
+  test("longestSubSequence('ABBA')", () => {
+    expect(longestSubSequence('ABBA')).toEqual(2);
+  });
+});
