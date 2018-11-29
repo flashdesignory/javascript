c161a16ae05816d9e3d4723ba14278a8280b8834
Thorsten Kober <thor@part4.com>
2018-11-28 16:45:07 -0800

    added test for trie

--- datastructures/trie.js
+++ datastructures/trie.js
@@ -38,49 +38,53 @@ class Trie {
     let current = this.root;
 
     for (let i = 0; i < word.length; i++) {
-      if (!current.children[word[i]]) {
+      const letter = word[i];
+      if (!current.children[letter]) {
         return false;
       }
-      current = current.children[word[i]];
+      current = current.children[letter];
     }
-
     return true;
   }
 
   findWords(word) {
     let current = this.root;
     const result = [];
-    let letters = '';
 
     for (let i = 0; i < word.length; i++) {
-      if (!current.children[word[i]]) {
+      const letter = word[i];
+      if (!current.children[letter]) {
         return result;
       }
-      current = current.children[word[i]];
-      letters += current.value;
+      current = current.children[letter];
     }
 
-    function traverse(node, chars) {
-      // eslint-disable-next-line
+    function traverse(node, letters) {
       Object.values(node.children).forEach((child) => {
-        chars += child.value;
+        letters += child.value;
         if (child.isLast) {
-          return result.push(chars);
+          return result.push(letters);
         }
-        traverse(child, chars);
-        chars = chars.substring(0, chars.length - 1);
+        traverse(child, letters);
+        letters = letters.substr(0, letters.length - 1);
+        return letters;
       });
     }
 
-    traverse(current, letters);
-
+    traverse(current, word);
     return result;
   }
 }
 
-const trie = new Trie();
-trie.insert('hello');
-trie.insert('hi');
-trie.insert('helographinator');
-trie.contains('hel');
-trie.findWords('h');
+// npx jest datastructures/trie.js
+describe('trie data structure', () => {
+  it('should return all options for letter h', () => {
+    const trie = new Trie();
+    const values = ['hello', 'dog', 'hi', 'helicopter', 'cat'];
+    values.sort();
+    for (let i = 0; i < values.length; i++) {
+      trie.insert(values[i]);
+    }
+    expect(trie.findWords('h')).toEqual(['helicopter', 'hello', 'hi']);
+  });
+});
