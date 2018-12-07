8ae61959cbb3259bcb201c5bc1443f05ec421136
Thorsten Kober <thor@part4.com>
2018-12-07 11:21:50 -0800

    added string algo

---
+++ algorithms/string.reverseWordInPlace.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Reverse words in string
+ * @description: String function to reverse words in place
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+function reverseWord(arr, start, end) {
+  while (start < end) {
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+function reverseInPlace(str) {
+  const chars = str.split('');
+
+  let start = 0;
+  for (let i = 0; i <= chars.length; i++) {
+    if (i === chars.length || chars[i] === ' ') {
+      reverseWord(chars, start, i - 1);
+      start = i + 1;
+    }
+  }
+  return chars.join('');
+}
+
+// npx jest algorithms/string.reverseWordInPlace.js
+test('test reverse words', () => {
+  expect(reverseInPlace('I am the good boy')).toEqual('I ma eht doog yob');
+});
---
+++ algorithms/string.reverseWordOrder.js
@@ -0,0 +1,40 @@
+/*
+ * @title: Reverse order of words
+ * @description: String function to reverse the order of words
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+function reverseWord(arr, start, end) {
+  while (start < end) {
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+function reverseWords(arr) {
+  arr = arr.reverse();
+
+  let start = 0;
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (i === arr.length || arr[i] === ' ') {
+      reverseWord(arr, start, i - 1);
+      start = i + 1;
+    }
+  }
+
+  return arr.join('');
+}
+
+// npx jest algorithms/string.reverseWordOrder.js
+test('test reverse words', () => {
+  const message = ['c', 'a', 'k', 'e', ' ',
+    'p', 'o', 'u', 'n', 'd', ' ',
+    's', 't', 'e', 'a', 'l'];
+
+  expect(reverseWords(message)).toEqual('steal pound cake');
+});
