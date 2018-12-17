51a3bb8947f6d799d2b0b6b5c15871ec73b6281e
Thorsten Kober <thor@part4.com>
2018-12-17 11:36:59 -0800

    added matrix algo

---
+++ algorithms/matrix.consecutive.js
@@ -0,0 +1,81 @@
+/*
+ * @title: Matrix Consecutive Path
+ * @description: find longest path
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+/* Given a n*n matrix where all numbers are distinct,
+ find the maximum length path (starting from any cell)
+ such that all cells along the path are in increasing
+ order with a difference of 1.
+
+ We can move in 4 directions from a given cell (i, j),
+ i.e., we can move to (i+1, j) or (i, j+1) or (i-1, j) or (i, j-1)
+ with the condition that the adjacent cells have a difference of 1.
+*/
+
+const rowNum = [-1, 0, 0, 1]; // results in left and right
+const colNum = [0, -1, 1, 0]; // results in top and bottom
+
+function isValid(row, column, rowLength, columnLength) {
+  return row >= 0 && row < rowLength && column >= 0 && column < columnLength;
+}
+
+function dfs(matrix, row, column, visited, length, output) {
+  // visited[row][column] = true;
+  for (let i = 0; i < 4; i++) {
+    const nextRow = row + rowNum[i];
+    const nextCol = column + colNum[i];
+
+    if (isValid(nextRow, nextCol, matrix.length, matrix[0].length)
+      && (!visited[nextRow][nextCol]
+      && matrix[row][column] + 1 === matrix[nextRow][nextCol])) {
+      output.push(matrix[nextRow][nextCol]);
+      length++;
+      length = dfs(matrix, nextRow, nextCol, visited, length, output);
+    }
+  }
+
+  return length;
+}
+
+function findLongestPath(matrix) {
+  const visited = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    visited[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      visited[i][j] = false;
+    }
+  }
+
+  let maxLength = 1;
+  let maxPoints = [];
+  let tempPoints = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!visited[i][j]) {
+        tempPoints = [matrix[i][j]];
+        const length = dfs(matrix, i, j, visited, 1, tempPoints);
+        if (length > maxLength) {
+          maxLength = length;
+          maxPoints = [...tempPoints];
+        }
+      }
+    }
+  }
+  console.log(maxPoints);
+  return maxLength;
+}
+
+// npx jest algorithms/matrix.consecutive.js
+test('findPathInMatrix()', () => {
+  const matrix = [
+    [1, 2, 9],
+    [5, 3, 8],
+    [4, 6, 7],
+  ];
+  expect(findLongestPath(matrix)).toEqual(4);
+});
---
+++ algorithms/matrix.wordsearch.js
@@ -0,0 +1,80 @@
+/*
+ * @title: Matrix word search
+ * @description: Find words from dictionary in matrix
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+const rowNum = [-1, -1, -1, 0, 0, 1, 1, 1];
+const colNum = [-1, 0, 1, -1, 1, -1, 0, 1];
+
+function dictionaryContains(str) {
+  const dictionary = ['GEEKS', 'FOR', 'QUIZ', 'GO'];
+  for (let i = 0; i < dictionary.length; i++) {
+    if (dictionary[i].indexOf(str) === 0 && str.length === dictionary[i].length) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isValid(row, column, rowLength, columnLength) {
+  return row >= 0 && row < rowLength && column >= 0 && column < columnLength;
+}
+
+function dfs(matrix, row, col, visited, current, result) {
+  visited[row][col] = true;
+  current += matrix[row][col];
+  if (dictionaryContains(current)) {
+    result.push(current);
+  }
+
+  for (let i = 0; i < 8; i++) {
+    const nextRow = row + rowNum[i];
+    const nextCol = col + colNum[i];
+
+    if (isValid(nextRow, nextCol, matrix.length, matrix[0].length) && !visited[nextRow][nextCol]) {
+      dfs(matrix, nextRow, nextCol, visited, current, result);
+    }
+  }
+
+
+  visited[row][col] = false;
+  current = current.substr(0, current.length - 1);
+}
+
+function findWords(matrix) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  const visited = [];
+  for (let i = 0; i < matrix.length; i++) {
+    visited[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      visited[i][j] = false;
+    }
+  }
+
+  const current = '';
+  const result = [];
+
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      dfs(matrix, i, j, visited, current, result);
+    }
+  }
+
+  return result;
+}
+
+// npx jest algorithms/matrix.wordsearch.js
+test('findWords()', () => {
+  const words = [
+    ['G', 'I', 'Z'],
+    ['U', 'E', 'K'],
+    ['Q', 'S', 'E']];
+
+  expect(findWords(words)).toEqual(
+    ['GEEKS', 'QUIZ'],
+  );
+});
