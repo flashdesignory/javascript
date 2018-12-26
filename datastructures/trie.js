9c69ef5f1ecf8f7b77473c22c5e4568576a7f9b8
Thorsten Kober <thor@part4.com>
2018-12-26 14:19:43 -0800

    added trie remove method

--- datastructures/trie.js
+++ datastructures/trie.js
@@ -34,6 +34,43 @@ class Trie {
     current.isLast = true;
   }
 
+  remove(node, word, index, parent) {
+    if (!node) return null;
+
+    if (index === word.length) {
+      if (node.isLast) node.isLast = false;
+    }
+
+    if (Object.keys(node.children).length === 0) {
+      const newChildren = {};
+      Object.keys(parent.children)
+        .filter(value => value !== node.value)
+        .forEach((child) => {
+          newChildren[child] = parent.children[child];
+        });
+      parent.children = newChildren;
+      node = null;
+      return node;
+    }
+
+    const char = word[index];
+    index += 1;
+    node.children[char] = this.remove(node.children[char], word, index, node);
+
+    if (Object.keys(node.children).length === 0 && !node.isLast) {
+      const newChildren = {};
+      Object.keys(parent.children)
+        .filter(value => value !== node.value)
+        .forEach((child) => {
+          newChildren[child] = parent.children[child];
+        });
+      parent.children = newChildren;
+      node = null;
+      return node;
+    }
+    return node;
+  }
+
   contains(word) {
     let current = this.root;
 
@@ -86,5 +123,15 @@ describe('trie data structure', () => {
       trie.insert(values[i]);
     }
     expect(trie.findWords('h')).toEqual(['helicopter', 'hello', 'hi']);
+  });
+  it('should return all options for letter h after removal of option', () => {
+    const trie = new Trie();
+    const values = ['hello', 'dog', 'hi', 'helicopter', 'cat'];
+    values.sort();
+    for (let i = 0; i < values.length; i++) {
+      trie.insert(values[i]);
+    }
+    trie.remove(trie.root, 'helicopter', 0);
+    expect(trie.findWords('h')).toEqual(['hello', 'hi']);
   });
 });
