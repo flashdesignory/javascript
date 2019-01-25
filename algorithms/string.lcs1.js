2b7dabfaec62de607058cb961a991a7e85a5aea7
Thorsten Kober <thor@part4.com>
2019-01-24 17:15:42 -0800

    add alternate lcs algo

--- algorithms/string.lcs1.js
+++ algorithms/string.lcs1.js
@@ -5,6 +5,7 @@
  * @email: info@flashdesignory.com
  */
 
+// time: O(n^2);
 function lcs(str1, str2, index1, index2) {
   if (index1 === 0 || index2 === 0) return 0;
   if (str1[index1 - 1] === str2[index2 - 1]) {
@@ -16,6 +17,28 @@ function lcs(str1, str2, index1, index2) {
   );
 }
 
+// time: O(mn);
+function lcs2(str1, str2) {
+  const result = [];
+  for (let i = 0; i <= str1.length; i++) {
+    result[i] = [];
+    for (let j = 0; j <= str2.length; j++) {
+      result[i][j] = 0;
+    }
+  }
+
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        result[i][j] = result[i - 1][j - 1] + 1;
+      } else {
+        result[i][j] = Math.max(result[i - 1][j], result[i][j - 1]);
+      }
+    }
+  }
+  return result[str1.length][str2.length];
+}
+
 // npx jest algorithms/string.lcs1.js
 test('lcs()', () => {
   const str1 = 'AGGTAB';
@@ -23,4 +46,5 @@ test('lcs()', () => {
   const index1 = str1.length;
   const index2 = str2.length;
   expect(lcs(str1, str2, index1, index2)).toEqual(4);
+  expect(lcs2(str1, str2)).toEqual(4);
 });
