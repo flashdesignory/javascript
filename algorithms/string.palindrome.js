8eb546082493c40e7ac93533c627e0c6cfa22c83
Thorsten Kober <thor@part4.com>
2019-01-30 11:30:35 -0800

    edit to palindrome

--- algorithms/string.palindrome.js
+++ algorithms/string.palindrome.js
@@ -20,7 +20,7 @@ function isPalindrome2(str) {
   if (str.length === 0) return true;
   if (str.length === 1) return true;
   if (str[0] === str[str.length - 1]) {
-    return isPalindrome(str.slice(1, str.length - 1));
+    return isPalindrome2(str.slice(1, str.length - 1));
   }
   return false;
 }
@@ -28,8 +28,14 @@ function isPalindrome2(str) {
 function isPalindrome3(str) {
   if (str.length === 1) return true;
   if (str.length === 2) return str[0] === str[1];
-  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  if (str[0] === str.slice(-1)) return isPalindrome3(str.slice(1, -1));
   return false;
+}
+
+function isPalindrome4(str) {
+  if (str.length <= 1) return true;
+  if (str.substr(0, 1) !== str.substr(str.length - 1, 1)) return false;
+  return isPalindrome4(str.substr(1, str.length - 2));
 }
 
 // npx jest algorithms/string.palindrome.js
@@ -46,4 +52,9 @@ test('isPalindrome2()', () => {
 test('isPalindrome3()', () => {
   expect(isPalindrome3('madam')).toBe(true);
   expect(isPalindrome3('fire')).toBe(false);
+});
+
+test('isPalindrome4()', () => {
+  expect(isPalindrome4('madam')).toBe(true);
+  expect(isPalindrome4('fire')).toBe(false);
 });
