16832ae67ba4402dec21e37e90a77956e721d3a1
Thorsten Kober <thor@part4.com>
2019-02-01 15:28:13 -0800

    edit to gcd

--- algorithms/number.greatest.js
+++ algorithms/number.greatest.js
@@ -1,11 +1,11 @@
 /*
  * @title: greatest common divisor
- * @description: find greatest common divisor in numbers
+ * @description: find greatest common divisor of two numbers
  * @author: Thorsten Kober
  * @email: info@flashdesignory.com
  */
 
-function greatestCommonDivisorOne(a, b) {
+function gcd1(a, b) {
   if (a < 2 || b < 2) return 1;
 
   let divisor = 2;
@@ -20,20 +20,42 @@ function greatestCommonDivisorOne(a, b) {
   return greatest;
 }
 
-function greatestCommonDivisorTwo(a, b) {
+function gcd2(a, b) {
   if (b === 0) {
     return a;
   }
 
-  return greatestCommonDivisorTwo(b, a % b);
+  return gcd2(b, a % b);
+}
+
+function gcd3(a, b) {
+  if (a === b) return a;
+  if (a > b) return gcd3(a - b, b);
+  return gcd3(a, b - a);
+}
+
+// euclid
+function gcd4(a, b) {
+  // simple way to ensure a is min and b is max
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  if (max % min === 0) return min;
+  return gcd4(min, max % min);
 }
 
 // npx jest algorithms/number.greatest.js
-describe('greatest common divisor', () => {
+describe('gcd1()', () => {
   test('greatestCommonDivisorOne', () => {
-    expect(greatestCommonDivisorOne(14, 21)).toEqual(7);
+    expect(gcd1(14, 21)).toEqual(7);
   });
-  test('greatestCommonDivisorTwo', () => {
-    expect(greatestCommonDivisorTwo(14, 21)).toEqual(7);
+  test('gcd2();', () => {
+    expect(gcd2(14, 21)).toEqual(7);
+  });
+  test('gcd3();', () => {
+    expect(gcd3(14, 21)).toEqual(7);
+  });
+  test('gcd4();', () => {
+    expect(gcd4(14, 21)).toEqual(7);
   });
 });
