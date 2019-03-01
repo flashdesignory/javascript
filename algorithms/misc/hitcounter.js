9cd9533590b153d4931c2fef62d6f9e25ed779a1
Thorsten Kober <thorsten@thorstenkober.com>
2019-03-01 11:25:49 -0800

    simple hitcounter

---
+++ algorithms/misc/hitcounter.js
@@ -0,0 +1,65 @@
+/*
+ * @title: Hit Counter
+ * @description: simple solution
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+class HitCounter {
+  constructor(range = 300) {
+    this.range = range;
+    this.times = [];
+    this.hits = [];
+
+    for (let i = 0; i < range; i++) {
+      this.times[i] = [];
+      this.hits[i] = [];
+    }
+  }
+
+  hit(time) {
+    const index = time % this.range;
+
+    if (this.times[index] !== time) {
+      this.times[index] = time;
+      this.hits[index]++;
+    } else {
+      this.hits[index]++;
+    }
+  }
+
+  getHits(time) {
+    let result = 0;
+
+    for (let i = 0; i < this.range; i++) {
+      if (time - this.times[i] < this.range) {
+        result += this.hits[i];
+      }
+    }
+
+    return result;
+  }
+}
+
+
+// npx jest algorithms/misc/hitcounter.js
+test('HitCounter', () => {
+  const counter = new HitCounter();
+  // hit at timestamp 1.
+  counter.hit(1);
+  // hit at timestamp 2.
+  counter.hit(2);
+  // hit at timestamp 3.
+  counter.hit(3);
+  // get hits at timestamp 4, should return 3.
+  counter.getHits(4);
+  // hit at timestamp 300.
+  counter.hit(300);
+  // get hits at timestamp 300, should return 4.
+  counter.getHits(300);
+  // get hits at timestamp 301, should return 3.
+  counter.getHits(301);
+  counter.hit(301);
+  counter.hit(302);
+  expect(counter.getHits(302)).toEqual(6);
+});
