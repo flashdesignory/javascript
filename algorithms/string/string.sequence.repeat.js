bb7234c0cf62aca91a793b163ddd2a251a3c2ebb
Thorsten Kober <thorsten@thorstenkober.com>
2019-03-21 17:52:45 -0700

    string algo lrs

--- algorithms/string/string.sequence.repeat.js
+++ algorithms/string/string.sequence.repeat.js
@@ -1,10 +1,32 @@
 /*
  * @title: String sequence
- * @description: longest sequence of repeating characters
+ * @description: Longest Repeating Subsequence
  * @author: Thorsten Kober
  * @email: info@flashdesignory.com
  */
 
+// recursive - indexOf
+function longestSubSequenceRepeating(str, index) {
+  if (index < 0) return 0;
+  if (str.indexOf(str[index]) !== index) {
+    return longestSubSequenceRepeating(str, index - 1) + 1;
+  }
+  return longestSubSequenceRepeating(str, index - 1);
+}
+
+// recursive - using two pointers
+function longestSubSequenceRepeating0(str, index1, index2) {
+  if (index1 === 0 || index2 === 0) return 0;
+  if (str[index1 - 1] === str[index2 - 1] && index1 !== index2) {
+    return 1 + longestSubSequenceRepeating0(str, index1 - 1, index2 - 1);
+  }
+  return Math.max(
+    longestSubSequenceRepeating0(str, index1, index2 - 1),
+    longestSubSequenceRepeating0(str, index1 - 1, index2),
+  );
+}
+
+// map - extra storage
 function longestSubSequenceRepeating1(str) {
   const seen = {};
 
@@ -26,6 +48,7 @@ function longestSubSequenceRepeating1(str) {
   return Object.keys(seen).length;
 }
 
+// tabulation
 function longestSubSequenceRepeating2(str) {
   const seen = [];
   const length = str.length; //eslint-disable-line
@@ -52,6 +75,16 @@ function longestSubSequenceRepeating2(str) {
 
 // npx jest algorithms/string/string.sequence.repeat.js
 describe('longest string sequence of repeating chars', () => {
+  test("longestSubSequenceRepeating('aabebcdd')", () => {
+    const str = 'aabebcdd';
+    const index = str.length - 1;
+    expect(longestSubSequenceRepeating(str, index)).toEqual(3);
+  });
+  test("longestSubSequenceRepeating0('aabebcdd')", () => {
+    const str = 'aabebcdd';
+    const index = str.length;
+    expect(longestSubSequenceRepeating0(str, index, index)).toEqual(3);
+  });
   test("longestSubSequenceRepeating1('aabebcdd')", () => {
     expect(longestSubSequenceRepeating1('aabebcdd')).toEqual(3);
   });
