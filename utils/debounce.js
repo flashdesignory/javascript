208fc47ae06c97c33142c09e651c87666929c7e6
Thorsten Kober <info@flashdesignory.com>
2019-03-23 07:20:40 -0700

    edit to debounce

--- utils/debounce.js
+++ utils/debounce.js
@@ -5,23 +5,41 @@
  * @email: info@flashdesignory.com
  */
 
-function debounce(func, wait, immediate) {
+function debounceSimple(fn, delay) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments; // eslint-disable-line
+    const later = function () {
+      fn.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, delay);
+  };
+}
+
+function debounce(fn, wait, immediate) {
   let timeout;
   return function (...args) {
     const context = this;
     const later = function () {
       timeout = null;
-      if (!immediate) func.apply(context, args);
+      if (!immediate) fn.apply(context, args);
     };
     const callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+    if (callNow) fn.apply(context, args);
   };
 }
 
 const debounced = debounce(() => {
-  console.log('yo debounced');
+  console.log('mouse move debounced');
+}, 250);
+
+const debouncedSimple = debounceSimple(() => {
+  console.log('mouse down debounced');
 }, 250);
 
 window.addEventListener('mousemove', debounced);
+window.addEventListener('mousedown', debouncedSimple);
