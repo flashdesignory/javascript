1e20b1053932f99770f8f26b7e59a19ea35e7374
Thorsten Kober <thorsten@thorstenkober.com>
2019-04-04 14:27:56 -0700

    added few more patterns

---
+++ patterns/observer.js
@@ -0,0 +1,43 @@
+/*
+ * @title: Observer Pattern
+ * @description: Simple example
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(obj) {
+    this.observers.push(obj);
+  }
+
+  unsubscribe(obj) {
+    this.observers = this.observers.filter(subscriber => subscriber !== obj);
+  }
+
+  notify(data) {
+    this.observers.forEach(subscriber => subscriber.update(data));
+  }
+}
+
+class Observer {
+  constructor(id) {
+    this.id = id;
+  }
+
+  update(data) {
+    console.log(this.id, 'update', data);
+  }
+}
+
+const subj = new Subject();
+const o1 = new Observer('foo');
+const o2 = new Observer('bar');
+const o3 = new Observer('baz');
+subj.subscribe(o1);
+subj.subscribe(o2);
+subj.subscribe(o3);
+subj.notify('hello world');
---
+++ patterns/singleton.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Singleton Pattern
+ * @description: Simple example
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+const singleton = (function () {
+  let instance;
+
+  function init() {
+    const id = Math.random() * 1000;
+
+    return {
+      getId: function () {
+        return id;
+      },
+    };
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    },
+  };
+})();
+
+const foo = singleton.getInstance();
+console.log(foo.getId());
+const bar = singleton.getInstance();
+console.log(bar.getId());
