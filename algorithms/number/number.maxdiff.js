ad16ea594d4bed2da710e70a6fe81a16d091ff15
Thorsten Kober <thorsten@thorstenkober.com>
2019-04-04 14:42:40 -0700

    added max diff

---
+++ algorithms/number/number.maxdiff.js
@@ -0,0 +1,29 @@
+/*
+ * @title: Max Difference
+ * @description: Maximum difference between
+ * two elements such that larger element appears after the smaller number
+ * @author: Thorsten Kober
+ * @email: info@flashdesignory.com
+ */
+
+// sliding window
+function maxDiff(arr) {
+  let max = arr[1] - arr[0];
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - min > max) {
+      max = arr[i] - min;
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return max;
+}
+
+// npx jest algorithms/number/number.maxDiff.js
+test('maxDiff()', () => {
+  expect(maxDiff([1, 2, 90, 10, 110])).toEqual(109);
+});
