cebba25ce4760bada828d68c72950a259161ac6b
Thorsten Kober <info@flashdesignory.com>
2019-04-28 08:23:56 -0700

    fixed array insert

--- algorithms/array/array.insert.js
+++ algorithms/array/array.insert.js
@@ -8,8 +8,8 @@
 function findIndexToInsert(arr, num) {
   arr.sort((a, b) => a - b);
   let index = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < num) {
       index++;
     }
   }
@@ -18,5 +18,7 @@ function findIndexToInsert(arr, num) {
 
 // npx jest algorithms/array/array.insert.js
 test('findIndexToInsert()', () => {
-  expect(findIndexToInsert([10, 20, 30, 40, 50], 35)).toEqual(2);
+  expect(findIndexToInsert([10, 20, 30, 40, 50], 35)).toEqual(3);
+  expect(findIndexToInsert([10, 20, 30, 40, 50], 2)).toEqual(0);
+  expect(findIndexToInsert([10, 20, 30, 40, 50], 60)).toEqual(5);
 });
