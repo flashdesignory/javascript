95dd0a4897b715ae8224c509b504a18d710f8875
Thorsten Kober <thorsten@thorstenkober.com>
2019-04-30 17:02:48 -0700

    edit roman string

--- algorithms/string/string.roman.js
+++ algorithms/string/string.roman.js
@@ -5,7 +5,7 @@
  * @email: info@flashdesignory.com
  */
 
-function roman(num) {
+function toRoman(num) {
   const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   const letters = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
@@ -21,7 +21,25 @@ function roman(num) {
   return result;
 }
 
+function fromRoman(str) {
+  const decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const letters = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  let result = 0;
+
+  for (let i = 0; i <= decimals.length; i++) {
+    while (str.indexOf(letters[i]) === 0) {
+      result += decimals[i];
+      str = str.replace(letters[i], '');
+    }
+  }
+  return result;
+}
+
 // npx jest algorithms/string/string.roman.js
-test('roman()', () => {
-  expect(roman(36)).toEqual('XXXVI');
+test('toRoman()', () => {
+  expect(toRoman(36)).toEqual('XXXVI');
+});
+test('fromRoman()', () => {
+  expect(fromRoman('III')).toEqual(3);
 });
