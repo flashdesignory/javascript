fc0db58b1b240e588668a86f8e1e078c2aaf308f
Thorsten Kober <info@flashdesignory.com>
2019-05-15 18:34:22 -0700

    min coin change db solution

--- algorithms/number/number.change.minimum.js
+++ algorithms/number/number.change.minimum.js
@@ -5,6 +5,7 @@
  * @email: info@flashdesignory.com
  */
 
+// recursion
 function minCoinChange(coins, amount) {
   const result = [];
   for (let i = coins.length - 1; i >= 0; i--) {
@@ -16,9 +17,27 @@ function minCoinChange(coins, amount) {
   return result;
 }
 
-minCoinChange([1, 2, 5, 10, 25], 30);
+// tabulation
+function minCoinChange2(coins, amount) {
+  const result = [];
+  for (let i = 1; i <= amount; i++) {
+    result[i] = Infinity;
+  }
+
+  result[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        result[i] = Math.min(result[i], result[i - coins[j]] + 1);
+      }
+    }
+  }
+  return result[amount] > amount ? -1 : result[amount];
+}
 
 // npx jest algorithms/number/number.change.minimum.js
 test('minCoinChange()', () => {
   expect(minCoinChange([1, 2, 5, 10, 25], 30)).toEqual([25, 5]);
+  expect(minCoinChange2([1, 2, 5, 10, 25], 30)).toEqual(2);
 });
