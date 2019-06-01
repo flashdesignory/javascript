36783d53b32c2318e0662bf720f9906da92a9932
Thorsten Kober <info@flashdesignory.com>
2019-06-01 14:33:40 -0700

    array insert

--- algorithms/array/array.insert.js
+++ algorithms/array/array.insert.js
@@ -16,9 +16,31 @@ function findIndexToInsert(arr, num) {
   return index;
 }
 
+function findIndexToInsertBS(arr, value) {
+  arr.sort((a, b) => a - b);
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    // start with left
+    if (arr[middle] < value) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return left;
+}
+
 // npx jest algorithms/array/array.insert.js
 test('findIndexToInsert()', () => {
   expect(findIndexToInsert([10, 20, 30, 40, 50], 35)).toEqual(3);
   expect(findIndexToInsert([10, 20, 30, 40, 50], 2)).toEqual(0);
   expect(findIndexToInsert([10, 20, 30, 40, 50], 60)).toEqual(5);
+});
+test('findIndexToInsertBS()', () => {
+  expect(findIndexToInsertBS([10, 20, 30, 40, 50], 35)).toEqual(3);
+  expect(findIndexToInsertBS([10, 20, 30, 40, 50], 2)).toEqual(0);
+  expect(findIndexToInsertBS([10, 20, 30, 40, 50], 60)).toEqual(5);
 });
