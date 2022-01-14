8b7df530606e8c2bad9229f66d45bef725cb0781
Thorsten Kober <tkober@gmail.com>
2022-01-14 14:34:03 -0800

    add comments to atoi

--- algorithms/string/string.atoi.js
+++ algorithms/string/string.atoi.js
@@ -7,37 +7,51 @@
  */
 
 function atoi(str) {
+  // pointer to loop through str
   let start = 0;
-  let res = 0;
+  // keeps result value
+  let result = 0;
+  // flag if input is negative
   let isNegative = false;
 
   // trim whitespace
   str = str.trim();
 
-  // increment if operator present
-  if (str[start] === '+' || str[start] === '-') {
-    if (str[start] === '-') {
+  // check if string starts with "+" or "-"
+  if (str[start] === "+" || str[start] === "-") {
+    if (str[start] === "-") {
       isNegative = true;
     }
-    start += 1;
+    start++;
   }
 
   for (let i = start; i < str.length; i++) {
-    const code = str.charCodeAt(i) - 48; // '0' is 48
-    console.log(code);
-    if (code < 0 || code > 9) {
+
+    // Valid number values range from 48 to 57.
+    // We want to append the current value to the result
+    // and therefore we don't want a range from 48 to 57
+    // but rather a range from 0 to 9, since that would be a valid digit
+    // Therefore we subtract 48 from the returned charCode value.
+    const charCode = str.charCodeAt(i) - 48;
+
+    // checking if charCode is within 0 to 9 range
+    if (charCode < 0 || charCode > 9) {
       break;
     }
-    res *= 10;
-    res += code;
+
+    // This ultimately adds a 0 to the end that we can add to.
+    result *= 10;
+    // This adds a value between 0 to 9 to the last digit.
+    result += charCode;
   }
 
+  // If the str started with a "-" sign, we multipy by -1.
+  if (isNegative) {
+    result *= -1;
+  }
 
-  // if negative
-  if (isNegative) res *= -1;
-
-  // make sure number is valid
-  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, res)); // eslint-disable-line
+  // Make sure number is in a valid range
+  return Math.max(-(2 ** 31), Math.min(2 ** 31 - 1, result));// eslint-disable-line
 }
 
 function atoi2(str) {
