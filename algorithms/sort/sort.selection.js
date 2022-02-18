c566c3529a995f7fb58b83354796cfd7f74bb658
Thorsten Kober <tkober@gmail.com>
2022-02-18 07:47:55 -0800

    sort cleanup

--- algorithms/sort/sort.selection.js
+++ algorithms/sort/sort.selection.js
@@ -7,20 +7,16 @@
 
 // Big O(n^2) time Big O(1) space
 function selectionSort(arr) {
-  const len = arr.length;
-  let min;
-  let temp;
-
-  for (let i = 0; i < len; i++) {
-    min = i;
-    for (let j = i + 1; j < len; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[min]) {
         min = j;
       }
     }
 
     if (i !== min) {
-      temp = arr[i];
+      const temp = arr[i];
       arr[i] = arr[min];
       arr[min] = temp;
     }
