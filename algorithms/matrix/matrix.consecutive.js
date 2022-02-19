0a2dfb6e7ebb36aa7fb30a29e30f3b16896bc3e9
Thorsten Kober <tkober@gmail.com>
2022-02-18 17:46:13 -0800

    simplify matrix

--- algorithms/matrix/matrix.consecutive.js
+++ algorithms/matrix/matrix.consecutive.js
@@ -57,13 +57,11 @@ function findLongestPath(matrix) {
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (!visited[i][j]) {
-        tempPoints = [matrix[i][j]];
-        const length = dfs(matrix, i, j, visited, 1, tempPoints);
-        if (length > maxLength) {
-          maxLength = length;
-          maxPoints = [...tempPoints];
-        }
+      tempPoints = [matrix[i][j]];
+      const length = dfs(matrix, i, j, visited, 1, tempPoints);
+      if (length > maxLength) {
+        maxLength = length;
+        maxPoints = [...tempPoints];
       }
     }
   }
