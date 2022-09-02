96ef13725d9bd0a89bbf1721c9d0def6ab900387
Thorsten Kober <tkober@gmail.com>
2022-09-02 08:23:03 -0700

    update throttle

--- utils/throttle.js
+++ utils/throttle.js
@@ -5,7 +5,9 @@
  * @email: info@flashdesignory.com
  */
 
-function throttle(fn, wait) {
+// simple throttle:
+// fires initial event and waits 
+function simple_throttle(fn, wait) {
   let last = 0;
   return function (...args) {
     const now = new Date().getTime();
@@ -18,7 +20,50 @@ function throttle(fn, wait) {
   };
 }
 
+// with leading and trailing options
+// leading: fires initial event
+// trailing: captures last event that might have been triggered too fast
+const throttle = (fn, delay, { leading = true, trailing = true } = {}) => {
+  let last = 0;
+  let timeout;
+  return function (...args) {
+      const now = new Date().getTime();
+      timeout ?? clearTimeout(timeout);
+
+      // if first event shouldn't fire, update last var early
+      if (!leading && last === 0) {
+          last = now;
+      }
+
+      if (now - last < delay) {
+          if (trailing) {
+              const difference = now - last;
+              const leftOverDelay = delay - difference;
+              timeout = setTimeout(() => fn.apply(this, args), leftOverDelay);
+          }
+          return;
+      }
+
+      last = now
+      fn.apply(this, args);
+  }
+}
+
 // npx jest utils/throttle.js
+test('utils.simple_throttle', () => {
+  console.log = jest.fn();
+  const throttled = simple_throttle(() => {
+    console.log('throttled');
+  }, 250);
+
+  // window.addEventListener('mousemove', throttled);
+
+  jest.useFakeTimers();
+  throttled();
+  jest.runOnlyPendingTimers();
+  expect(console.log).toHaveBeenCalledTimes(1);
+});
+
 test('utils.throttle', () => {
   console.log = jest.fn();
   const throttled = throttle(() => {
